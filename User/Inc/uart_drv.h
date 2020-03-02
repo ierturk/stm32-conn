477d2940d4c2966624dddcf7d9a0cc899f99a53c
Ibrahim ERTURK <ierturk@ieee.org>
2020-03-02 18:49:19 +0300

    Initial commit

---
+++ User/Inc/uart_drv.h
@@ -0,0 +1,21 @@
+/*
+ * uar_drv.h
+ *
+ *  Created on: Feb 12, 2020
+ *      Author: ierturk
+ */
+
+#ifndef UART_DRV_H_
+#define UART_DRV_H_
+
+#define UART6_DMA_RX_BUFF_SIZE (256)
+
+typedef struct {
+	uint8_t *buff;
+	uint16_t len;
+	uint32_t id;
+} tiva_msg_t;
+
+void init_uart_drv(void);
+
+#endif /* UART_DRV_H_ */
---
+++ User/Src/uart_drv.c
@@ -0,0 +1,72 @@
+/*
+ * AppMain.cpp
+ *
+ *  Created on: Feb 12, 2020
+ *      Author: ierturk
+ */
+
+#include <stdint.h>
+#include "cmsis_os.h"
+#include "main.h"
+#include "uart_drv.h"
+
+#include "usart.h"
+
+extern DMA_HandleTypeDef hdma_usart6_rx;
+extern UART_HandleTypeDef huart6;
+extern osMessageQueueId_t tiva_msgHandle;
+
+static uint8_t rxBuff[UART6_DMA_RX_BUFF_SIZE] = {};
+
+void init_uart_drv(void) {
+
+	__HAL_UART_ENABLE_IT(&huart6, UART_IT_IDLE);
+	HAL_UART_Receive_DMA(&huart6, (uint8_t*)rxBuff, UART6_DMA_RX_BUFF_SIZE);
+
+}
+
+ void uart6_dma_usart_rx_check(void) {
+     static uint32_t msg_id = 0;
+	 static size_t old_pos;
+     size_t pos;
+     tiva_msg_t msg;
+
+     pos = UART6_DMA_RX_BUFF_SIZE - __HAL_DMA_GET_COUNTER(&hdma_usart6_rx);
+
+     if (pos != old_pos) {
+         if (pos > old_pos) {
+        	 msg.len = pos - old_pos;
+        	 msg.buff = &rxBuff[old_pos];
+        	 msg.id = msg_id++;
+        	 osMessageQueuePut(tiva_msgHandle, &msg, 0U, 0U);
+
+
+         } else {
+        	 msg.len = UART6_DMA_RX_BUFF_SIZE - old_pos;
+        	 msg.buff = &rxBuff[old_pos];
+        	 msg.id = msg_id++;
+        	 osMessageQueuePut(tiva_msgHandle, &msg, 0U, 0U);
+
+             if (pos) {
+            	 msg.len = pos;
+            	 msg.buff = &rxBuff[0];
+            	 msg.id = msg_id++;
+            	 osMessageQueuePut(tiva_msgHandle, &msg, 0U, 0U);
+             }
+         }
+     }
+     old_pos = pos;
+
+     if (old_pos == UART6_DMA_RX_BUFF_SIZE) {
+         old_pos = 0;
+     }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	uart6_dma_usart_rx_check();
+}
+
+void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart) {
+	uart6_dma_usart_rx_check();
+}
+
